13432642750e23def6cee1975dd117c090a99106
jav <javierlopeztl1@gmail.com>
2026-02-27 00:01:48 +0100

    fix: add environments properties

---
+++ src/main/java/com/videogamemanager/videogamemanager/exceptions/InvalidGameException.java
@@ -0,0 +1,11 @@
+package com.videogamemanager.videogamemanager.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidGameException extends RuntimeException {
+    public InvalidGameException(String message) {
+        super(message);
+    }
+}
