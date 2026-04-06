7c2fdc368f04b37803b18e6765d45a6b58d25186
jav <javierlopeztl1@gmail.com>
2026-04-06 11:52:42 +0200

    wip

--- src/main/java/com/videogamemanager/videogamemanager/models/dto/GameAdminDto.java
+++ src/main/java/com/videogamemanager/videogamemanager/models/dto/GameAdminDto.java
@@ -1,10 +1,16 @@
 package com.videogamemanager.videogamemanager.models.dto;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.time.LocalDateTime;
 
 @Data
 public class GameAdminDto {
+
+    @Id
+    @Field("_id")
     private String id; // El ID de MongoDB que el Admin necesita
     private String title;
     private String genre;
--- src/main/java/com/videogamemanager/videogamemanager/services/impl/GameServiceImpl.java
+++ src/main/java/com/videogamemanager/videogamemanager/services/impl/GameServiceImpl.java
@@ -9,12 +9,10 @@ import com.videogamemanager.videogamemanager.repository.GameRepository;
 import com.videogamemanager.videogamemanager.services.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
 import org.springframework.data.domain.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
-import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -88,11 +86,11 @@ public class GameServiceImpl implements GameService {
                 .count().as("totalGames")
                 .avg("age")
                 .as("averageAge")
-                .push(Aggregation.ROOT).as("games");
+                .push(Aggregation.ROOT).as("game");
         // 2. Proyectar el resultado al DTO ($project)
         ProjectionOperation projectToDto = Aggregation.project()
                 .andExpression("_id").as("genre")
-                .andInclude("totalGames", "averageAge","games");
+                .andInclude("totalGames", "averageAge","game");
 
         // 3. Ordenar por cantidad de juegos descendente ($sort)
         SortOperation sortByTotal = Aggregation.sort(Sort.Direction.DESC, "totalGames");
@@ -103,7 +101,10 @@ public class GameServiceImpl implements GameService {
                 sortByTotal
         );
 
-        return mongoTemplate.aggregate(aggregation, "games", GameStatsDto.class).getMappedResults();
+        AggregationResults<Document> rawResults = mongoTemplate.aggregate(aggregation, "games", Document.class);
+        log.info("RESULTADO RAW DE MONGO: {}", rawResults.getMappedResults());
+
+        return mongoTemplate.aggregate(aggregation, "game", GameStatsDto.class).getMappedResults();
 
     }
 
