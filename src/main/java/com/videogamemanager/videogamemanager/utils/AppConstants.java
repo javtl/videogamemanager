4e1f145c1468911dcef787eb78e27d21f86e5e41
jav <javierlopeztl1@gmail.com>
2026-04-12 18:34:57 +0200

    feat: implement AppConstants and utility class structure
    - Created AppConstants in utils package to centralize response messages and default values.
    - Implemented private constructor with IllegalStateException to prevent instantiation of utility classes.
    - Refactored hardcoded strings for pagination and status messages to use AppConstants.

---
+++ src/main/java/com/videogamemanager/videogamemanager/utils/AppConstants.java
@@ -0,0 +1,15 @@
+package com.videogamemanager.videogamemanager.utils;
+
+public class AppConstants {
+
+    private AppConstants(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String MSG_GAME_DELETE = "Videojuego eliminado con exito";
+    public static final String MSG_GAME_NOT_FOUND = "Videojuego no encontrado con exito";
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+}
